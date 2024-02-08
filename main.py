ff17e329f9c50a9f2a70efe0cead67e476626a88
Ruhomaun Khaleelullah Ahmad Raza Qadri <105217847+Cxde02@users.noreply.github.com>
2024-02-08 11:08:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from uiColors import *
 #Labels
 clockLabel = uiClockLabel
 dateLabel = uiDateLabel
-clockLabel.place(x= 10, y = 20)
-dateLabel.place(x= 10, y = 90)
+clockLabel.place(x = 10, y = 20)
+dateLabel.place(x = 10, y = 90)
 
 def updateLabel():
-    currentTime = strftime( '%H:%M:%S')     #Getting current hour, minutes and seconds
+    currentTime = strftime('%H:%M:%S')     #Getting current hour, minutes and seconds
     dateToday = strftime("%A, %d %B %Y")      #Getting current day, date and month
-    clockLabel.config(text=currentTime)        #Setting text to show current time
-    dateLabel.config(text=dateToday)          #Setting text to show current date
+    clockLabel.config(text = currentTime)        #Setting text to show current time
+    dateLabel.config(text = dateToday)          #Setting text to show current date
     window.after(1000, updateLabel)           #Updating every second (1000 milliseconds)
 
 updateLabel()
